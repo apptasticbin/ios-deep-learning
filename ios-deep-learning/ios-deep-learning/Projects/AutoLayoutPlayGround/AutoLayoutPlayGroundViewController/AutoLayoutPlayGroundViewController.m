e107ffb920df993fafa719b9ffd96d6946c160b3
Bin Yu <binyu.fi@gmail.com>
2016-12-13 23:57:45 +0200

    AutoLayoutPlayGroundViewController code refactoring.

--- ios-deep-learning/ios-deep-learning/Projects/AutoLayoutPlayGround/AutoLayoutPlayGroundViewController/AutoLayoutPlayGroundViewController.m
+++ ios-deep-learning/ios-deep-learning/Projects/AutoLayoutPlayGround/AutoLayoutPlayGroundViewController/AutoLayoutPlayGroundViewController.m
@@ -26,6 +26,8 @@
 @property (nonatomic, strong) AutoLayoutRect *blueRect;
 @property (nonatomic, strong) AutoLayoutRect *greenRect;
 
+@property (nonatomic, strong) NSLayoutConstraint *blueRectHeightConstraint;
+
 @end
 
 @implementation AutoLayoutPlayGroundViewController
@@ -68,6 +70,12 @@
     MarkEnd;
 }
 
+- (void)updateViewConstraints {
+    MarkStart;
+    [super updateViewConstraints];
+    MarkEnd;
+}
+
 #pragma mark - Initialization
 
 - (void)initializeViews {
@@ -83,93 +91,148 @@
     [self.redRect addSubviewWithoutAutoResizing:self.greenRect];
     // orange rect is child of blue rect
     [self.blueRect addSubviewWithoutAutoResizing:self.orangeRect];
-    
     [self.playStage addSubviewWithoutAutoResizing:self.redRect];
 }
 
 - (void)initializeViewConstraints {
     [super initializeViewConstraints];
     
-    NSDictionary<NSString *, __kindof UIView *> *views = @{
-                                                           @"redRect"       : self.redRect,
-                                                           @"orangeRect"    : self.orangeRect,
-                                                           @"blueRect"      : self.blueRect,
-                                                           @"greenRect"     : self.greenRect
-                                                           };
     // Auto Layout Visual Format Syntax
-    NSArray *redRectHorizontalConstraints =
-    [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-40-[redRect]-40-|"
-                                            options:0
-                                            metrics:nil
-                                              views:views];
-    [self.playStage addConstraints:redRectHorizontalConstraints];
+    // red rect constraints
+    [self.playStage addConstraints:[self redRectConstraints]];
     
-    NSArray *redRectVerticalConstraints =
-    [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-40-[redRect]-40-|"
-                                            options:0
-                                            metrics:nil
-                                              views:views];
-    [self.playStage addConstraints:redRectVerticalConstraints];
+    // blue rect constraints
+    [self.redRect addConstraints:[self blueRectConstraints]];
     
-    NSArray *blueRectHorizontalConstraints =
-    [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-30-[blueRect(150)]"
-                                            options:0
-                                            metrics:nil
-                                              views:views];
-    [self.redRect addConstraints:blueRectHorizontalConstraints];
+    // green rect constraints
+    [self.redRect addConstraints:[self greenRectConstraints]];
     
-    NSArray *blueRectVerticalConstraints =
-    [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-30-[blueRect(150)]"
-                                            options:0
-                                            metrics:nil
-                                              views:views];
-    [self.redRect addConstraints:blueRectVerticalConstraints];
-    
-    NSArray *greenRectHorizontalConstraints =
-    [NSLayoutConstraint constraintsWithVisualFormat:@"H:[greenRect(100)]-30-|"
-                                            options:0
-                                            metrics:nil
-                                              views:views];
-    [self.redRect addConstraints:greenRectHorizontalConstraints];
-    
-    NSArray *greenRectVerticalConstraints =
-    [NSLayoutConstraint constraintsWithVisualFormat:@"V:[greenRect(100)]-30-|"
-                                            options:0
-                                            metrics:nil
-                                              views:views];
-    [self.redRect addConstraints:greenRectVerticalConstraints];
-    
-    NSArray *ornageRectHorizontalConstraints =
-    [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-30-[orangeRect(50)]"
-                                            options:0
-                                            metrics:nil
-                                              views:views];
-    [self.blueRect addConstraints:ornageRectHorizontalConstraints];
-    
-    NSArray *ornageRectVerticalConstraints =
-    [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-30-[orangeRect(50)]"
-                                            options:0
-                                            metrics:nil
-                                              views:views];
-    [self.blueRect addConstraints:ornageRectVerticalConstraints];
+    // orange rect constraints
+    [self.blueRect addConstraints:[self orangeRectConstraints]];
     
 }
 
 - (void)initializeControlPanel {
     [super initializeControlPanel];
-    self.controlActions = @[
-                            [[PlayGroundControlAction alloc] initWithName:@"Check Ambiguous Layout"
-                                                                   target:self
-                                                                   action:@selector(checkAmbiguousLayout)]
-                            ];
+    self.controlActions = [self controlPanelActions];
 }
 
 #pragma mark - Private
+
+- (NSArray<PlayGroundControlAction *> *)controlPanelActions {
+    return @[
+             [[PlayGroundControlAction alloc] initWithName:@"Check Ambiguous Layout"
+                                                    target:self
+                                                    action:@selector(checkAmbiguousLayout)],
+             [[PlayGroundControlAction alloc] initWithName:@"Move Orange From Blue To Green"
+                                                    target:self
+                                                    action:@selector(moveOrangeFromBlueToGreen)],
+             [[PlayGroundControlAction alloc] initWithName:@"Resize Blue"
+                                                    target:self
+                                                    action:@selector(resizeBlueRect)]
+             ];
+}
+
+- (NSDictionary<NSString *, __kindof UIView *> *)playGroundViews {
+    return @{
+             @"redRect"       : self.redRect,
+             @"orangeRect"    : self.orangeRect,
+             @"blueRect"      : self.blueRect,
+             @"greenRect"     : self.greenRect
+             };
+}
 
 - (void)checkAmbiguousLayout {
     if ([self.view hasAmbiguousLayout]) {
         [self.view exerciseAmbiguityInLayout];
     }
+}
+
+- (void)moveOrangeFromBlueToGreen {
+    AutoLayoutRect *orangeSuperView = (AutoLayoutRect *)self.orangeRect.superview;
+    AutoLayoutRect *targetSuperView = (orangeSuperView == self.blueRect ? self.greenRect : self.blueRect);
+    // when we adjust the parent of subview, all constraints will be removed.
+    [self.orangeRect removeFromSuperview];
+    [targetSuperView addSubviewWithoutAutoResizing:self.orangeRect];
+    // now add constraints to new superview
+    [targetSuperView addConstraints:[self orangeRectConstraints]];
+}
+
+- (void)resizeBlueRect {
+    [UIView animateWithDuration:1.0f animations:^{
+        self.blueRectHeightConstraint.constant = self.blueRectHeightConstraint.constant > 150.0f ? 150.0f : 250.0f;
+        [self.redRect layoutIfNeeded];
+    }];
+}
+
+#pragma mark - Constraints
+
+- (NSArray *)redRectConstraints {
+    NSArray *redRectHorizontalConstraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-40-[redRect]-40-|"
+                                            options:0
+                                            metrics:nil
+                                              views:[self playGroundViews]];
+    
+    NSArray *redRectVerticalConstraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-40-[redRect]-40-|"
+                                            options:0
+                                            metrics:nil
+                                              views:[self playGroundViews]];
+    
+    return [redRectHorizontalConstraints arrayByAddingObjectsFromArray:redRectVerticalConstraints];
+}
+
+- (NSArray *)blueRectConstraints {
+    NSArray *blueRectHorizontalConstraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-30-[blueRect(150)]"
+                                            options:0
+                                            metrics:nil
+                                              views:[self playGroundViews]];
+    
+    NSArray *blueRectVerticalConstraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-30-[blueRect]"
+                                            options:0
+                                            metrics:nil
+                                              views:[self playGroundViews]];
+    self.blueRectHeightConstraint =
+    [NSLayoutConstraint constraintWithItem:self.blueRect attribute:NSLayoutAttributeHeight
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:nil
+                                 attribute:NSLayoutAttributeNotAnAttribute
+                                multiplier:1.0
+                                  constant:150];
+    return [blueRectHorizontalConstraints arrayByAddingObjectsFromArray:[blueRectVerticalConstraints arrayByAddingObject:self.blueRectHeightConstraint]];
+}
+
+- (NSArray *)greenRectConstraints {
+    NSArray *greenRectHorizontalConstraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-150-[greenRect]-20-|"
+                                            options:0
+                                            metrics:nil
+                                              views:[self playGroundViews]];
+    
+    NSArray *greenRectVerticalConstraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-150-[greenRect]-20-|"
+                                            options:0
+                                            metrics:nil
+                                              views:[self playGroundViews]];
+    return [greenRectHorizontalConstraints arrayByAddingObjectsFromArray:greenRectVerticalConstraints];
+}
+
+- (NSArray *)orangeRectConstraints {
+    NSArray *orangeRectHorizontalConstraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-30-[orangeRect(50)]"
+                                            options:0
+                                            metrics:nil
+                                              views:[self playGroundViews]];
+    
+    NSArray * orangeRectVerticalConstraints =
+    [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-30-[orangeRect(50)]"
+                                            options:0
+                                            metrics:nil
+                                              views:[self playGroundViews]];
+    return [orangeRectHorizontalConstraints arrayByAddingObjectsFromArray:orangeRectVerticalConstraints];
 }
 
 #pragma mark - Project
